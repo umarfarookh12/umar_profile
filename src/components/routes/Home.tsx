ed9b0140e458123bc708ca2425c5f02ca33de56f
umarfarookh <sdumarfarookh212@gmail.com>
2024-07-12 14:26:45 +0530

    navLink folder ---> routes

---
+++ src/components/routes/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="border border-black">
+      <h1 className="text-2xl border border-black">Hero page</h1>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/routes/MyProjects.tsx
+++ src/components/routes/MyProjects.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const MyProjects = () => {
   return <div>MyProjects</div>;
 };

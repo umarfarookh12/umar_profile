e36cea8a84f159588248edf69ad23856308249d2
umarfarookh <sdumarfarookh212@gmail.com>
2024-07-12 14:18:59 +0530

    added react router for about, home etc and used router provider and removed browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import { BrowserRouter } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeProvider";
 
+import Home from "./components/NavLinks/Home";
+import About from "./components/NavLinks/About";
+import MyProjects from "./components/NavLinks/MyProjects";
+import Contact from "./components/NavLinks/Contact";
+import NavBar from "./components/NavBar";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <NavBar />
+          <Outlet />
+        </>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/About",
+          element: <About />,
+        },
+        {
+          path: "/myProjects",
+          element: <MyProjects />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ]);
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <NavBar />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }

431366b3cbca950ca341a84bce1df2f717b9cc4d
umarfarookh <sdumarfarookh212@gmail.com>
2024-08-07 23:19:48 +0530

    loading for page loading

---
+++ src/components/Progess.tsx
@@ -0,0 +1,18 @@
+type progessbar = {
+  pNumber: string;
+};
+const Progess = (props: progessbar) => {
+  const { pNumber } = props;
+  return (
+    <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700 ">
+      <div
+        className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.4 leading-none rounded-full"
+        style={{ width: `${pNumber}%` }}
+      >
+        {pNumber}%
+      </div>
+    </div>
+  );
+};
+
+export default Progess;

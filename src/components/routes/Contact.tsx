9902ecb78a7d3f9e2ee63aea3ed2907aa02adf34
umar <sdumarfarookh212@gmail.com>
2024-08-27 12:02:57 +0530

    updated styles

--- src/components/routes/Contact.tsx
+++ src/components/routes/Contact.tsx
@@ -8,21 +8,23 @@ import {
 const Contact = () => {
   return (
     <div>
-      <Card className="mx-auto my-3 md:w-[100%]">
+      <Card className="mx-auto my-3 space-y-10 md:w-[100%] border-none ">
         <CardHeader>
-          <CardTitle className="text-3xl md:text-md font-allura text-blue-600 dark:text-blue-400 tracking-widest text-center ">
+          <CardTitle className="text-3xl md:text-md font-allura text-blue-600 dark:text-blue-400 tracking-widest text-center">
             Contact me
           </CardTitle>
         </CardHeader>
-        <CardContent className="space-y-6 w-full border border-black mx-auto">
-          <div className="flex flex-col items-center w-full">
+        <CardContent className="space-y-10 w-full mx-auto">
+          <div className="flex flex-col items-center w-full space-y-10">
             <p className="md:shrink-0 sm:shrink text-xl md:text-2xl font-semibold text-black dark:text-blue-400">
               Ways To Contact Me
             </p>
-            <p className="text-sm font-semibold text-muted-foreground md:shrink-0 sm:shrink">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatibus eius officiis totam! Quod, officiis dolorem! Saepe
-              nobis
+            <p className="text-sm font-semibold text-muted-foreground md:shrink-0 sm:shrink md:w-[70%] space-y-10">
+              For any inquiries or further communication, please don't hesitate
+              to reach out to me. You can contact me via email, or if you
+              prefer, you can call or text me , I am also available on [mention
+              any other platforms like LinkedIn or WhatsApp, and I will do my
+              best to respond promptly
             </p>
           </div>
           <div className="grid md:grid-cols-2 justify-items-center content-center place-content-center md:w-[70%] md:mx-auto gap-4 text-sm">

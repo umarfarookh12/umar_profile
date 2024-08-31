10057bab20eb8a536a990b2d0e4dc7120256c718
umar <sdumarfarookh212@gmail.com>
2024-08-31 14:26:00 +0530

    removed loading from home

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -1,58 +1,42 @@
-// import { Button } from "../ui/button";
-// import { Card } from "../ui/card";
-
-import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(false);
-  }, []);
   return (
     <>
-      {loading ? (
-        <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-blue-600"></div>
-      ) : (
-        <div className="relative px-6 ">
-          <div className="mx-auto max-w-2xl py-32 sm:py-48">
-            <div className="text-center">
-              <h1 className="text-4xl font-bold xt-gray-600 text-black tracking-wider uppercase sm:text-6xl">
-                Umar farookh <span className="font-allura">syed</span>
-              </h1>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                Building responsive, dynamic, and user-friendly web applications
-              </p>
-              <div className="mt-10 flex items-center justify-center gap-x-6">
-                <div className="flex flex-wrap justify-center gap-6">
-                  <a className="relative" href="#">
-                    <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
-                    <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900">
-                      <a
-                        href="src/umar_front_end_0.5Intern.pdf"
-                        download={true}
-                      >
-                        Download resume
-                      </a>
-                    </span>
-                  </a>
-                  <a href="#" className="relative">
-                    <NavLink to="/About">
-                      <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-700"></span>
-                      <button>
-                        <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-black px-3 py-1 text-base font-bold text-white transition duration-100 hover:bg-gray-900 hover:text-yellow-500 ">
-                          let's get started &rarr;
-                        </span>
-                      </button>
-                    </NavLink>
-                  </a>
-                </div>
+      <div className="relative px-6 ">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold xt-gray-600 text-black tracking-wider uppercase sm:text-6xl">
+              Umar farookh <span className="font-allura">syed</span>
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Building responsive, dynamic, and user-friendly web applications
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <div className="flex flex-wrap justify-center gap-6">
+                <a className="relative" href="#">
+                  <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+                  <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900">
+                    <a href="src/umar_front_end_0.5Intern.pdf" download={true}>
+                      Download resume
+                    </a>
+                  </span>
+                </a>
+                <a href="#" className="relative">
+                  <NavLink to="/About">
+                    <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-700"></span>
+                    <button>
+                      <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-black px-3 py-1 text-base font-bold text-white transition duration-100 hover:bg-gray-900 hover:text-yellow-500 ">
+                        let's get started &rarr;
+                      </span>
+                    </button>
+                  </NavLink>
+                </a>
               </div>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
